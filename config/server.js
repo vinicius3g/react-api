6640bb1fd1b6891f5067aed0b386632b784bc327
vinicius.paixao <vinicius.paixao@acct.global>
2021-05-10 09:58:20 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'af14037a10bfa39ab5e1f88b8ae25111'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23c8e142-c529-4d78-ae26-bd310193c270'
+};
