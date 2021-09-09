ee61de476903a94594dd3acffa49bb997cc44569
vinicius.paixao <vinicius.paixao@acct.global>
2021-09-09 20:02:02 -0300

    Changes to my-project noted

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'postgres'),
         username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '******'),
+        password: env('DATABASE_PASSWORD', '159753'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
